817124a9071635b4bbd094402e62799308c760b8
vagrant <vagrant@vagrant.vm>
2020-10-26 23:03:41 +0000

    initial commit

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, jsonify, request
+from model import db, connect_to_db, Card
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def show_homepage():
+    """Show the application's homepage."""
+
+    return render_template("homepage.html")
+
+@app.route("/newPartData")
+def show_cards():
+    """Show all trading cards."""
+
+    return render_template("newPartData.html")
+
+
+if __name__ == "__main__":
+  # connect_to_db(app)
+    app.run(debug=True, host='0.0.0.0')
